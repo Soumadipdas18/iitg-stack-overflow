6aa081b9a7d4f94548a8ac8edeec3b314185298e
Soumadip Das <soumadipdas18@yahoo.com>
2021-08-09 18:13:13 +0530

    Update Config.js

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,5 +1,5 @@
 export const config = {
-  clientId: "ff607b18-dcf4-47db-8fa6-6258f68371fa",
+  clientId: "",
   redirectUri: "https://determined-hopper-c48e23.netlify.app/",
   scopes: ["user.read"],
 };
